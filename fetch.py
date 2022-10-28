307788495028e4aad71dca73c28ea3f728479403
Kent Zhang <kzhang5726@gmail.com>
2022-10-28 16:22:00 -0700

    Add rest of 2018

--- fetch.py
+++ fetch.py
@@ -1,13 +1,13 @@
 import requests
 
-api_keys = ["ccaf284e2c7322650b4d22edc05ff7b4", "423878ba73d398385d87b8559c20e1c6", "cb433c0cde9836c90855b92accd64610", "68dc4fe85dbc39ec214f7aedc31d7f2a"]
+api_keys = ["ccaf284e2c7322650b4d22edc05ff7b4", "423878ba73d398385d87b8559c20e1c6", "cb433c0cde9836c90855b92accd64610", "68dc4fe85dbc39ec214f7aedc31d7f2a", "f28908d63b6e854b52b894fa2f8b366e", "e02e1773d2da251380a9c73ee38396e8"]
 
-#years = list(range(2018,2023)
+# years = list(range(2018,2023)
 years = [2018]
-#months = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"]
-months = ["05", "06"]
+# months = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"]
+months = ["10", "11", "12"]
 days = ["01", "11", "21"]
-#days = ["21"]
+# days = ["01", "11"]
 
 
 counter = 0
@@ -19,8 +19,8 @@ for year in years:
             date_fields = [str(year), month, day]
             date_string = "-".join(date_fields)
             counter += 1
+            api_key = api_keys[i%6]
             i += 1
-            api_key = api_keys[i%4]
             url = "https://firms.modaps.eosdis.nasa.gov/api/area/csv/{key}/MODIS_SP/world/10/{date}".format(key=api_key, date=date_string)
             print("Making request for date: ", date_string)
             response = requests.get(url)
